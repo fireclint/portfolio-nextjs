dc913f829feb3c86cc0996b71c8693391cd51b70
Clint Briley <firebriley@Clints-MacBook-Pro.local>
2022-09-28 12:33:18 -0500

    updated about x2

--- components/About.jsx
+++ components/About.jsx
@@ -14,11 +14,11 @@ const About = () => {
           <h2 className='py-4'>Who I Am</h2>
           <p className='py-2 text-gray-600'>
             I specialize in building mobile responsive front-end UI applications
-            that connect with API’s &quot; other backend technologies. I’m
+            that connect with API’s and other backend technologies. I’m
             passionate about learning new technologies and understand there is
             more than one way to accomplish a task. Though I am most proficient
-            in building front-end applications using HTML, CSS, Javascript,
-            &quot; React, I am a quick learner and can pick up new tech stacks as
+            in building front-end applications using HTML, CSS, Javascript, and
+            React, I am a quick learner and can pick up new tech stacks as
             needed. I believe that being a great developer is not using one
             specific language, but choosing the best tool for the job.
           </p>
